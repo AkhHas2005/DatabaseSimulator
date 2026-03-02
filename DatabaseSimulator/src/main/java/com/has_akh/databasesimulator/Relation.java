55a00f1bec839267a3408989506edab0a1b30339
Python Programmer <151996864+HasanBradfordUni@users.noreply.github.com>
2026-03-02 12:21:44 +0000

    Further code development (V0.6)
    Relation class methods implemented and Tuple class extended

--- DatabaseSimulator/src/main/java/com/has_akh/databasesimulator/Relation.java
+++ DatabaseSimulator/src/main/java/com/has_akh/databasesimulator/Relation.java
@@ -9,47 +9,56 @@ import java.util.function.Predicate;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Hasan Akhtar
  */
 public class Relation {
+
     private String name;
     private List<Attribute> columns;
     private List<Tuple> records;
-    
+
     /**
      *
      * @param name - name of the table in the database
-     * @param columns - list of columns that will be part of the database table (column name and data type)
+     * @param columns - list of columns that will be part of the database table
+     * (column name and data type)
      */
     public Relation(String name, List<Attribute> columns) {
         this.name = name;
         this.columns = columns;
         this.records = new ArrayList<>();
     }
-    
-    public void insert(Tuple tuple){
+
+    public void insert(Tuple tuple) {
         records.add(tuple);
     }
-    
-    public void delete(String primaryKey) {
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).getPrimaryKey().equalsIgnoreCase(primaryKey)) {
-                records.remove(i);
+
+    public void delete(Predicate<Tuple> condition) {
+        records.removeIf(condition);
+    }
+
+    public void update(Predicate<Tuple> condition, Map<String, Object> newValues) {
+        for (Tuple t : records) {
+            if (condition.test(t)) {
+                for (Map.Entry<String, Object> entry : newValues.entrySet()) {
+                    t.set(entry.getKey(), entry.getValue());
+                }
             }
         }
     }
-    
-    public void update(Predicate<Tuple> condition, Map<String, Object> newValues) {
-        
+
+    public List<Tuple> select(Predicate<Tuple> condition) {
+        List<Tuple> result = new ArrayList<>();
+        for (Tuple t : records) {
+            if (condition.test(t)) {
+                result.add(t);
+            }
+        }
+        return result;
     }
-    
-    public void select(Predicate<Tuple> condition) {
-        
-    }
-    
+
     public String getName() {
         return name;
     }
--- DatabaseSimulator/src/main/java/com/has_akh/databasesimulator/Tuple.java
+++ DatabaseSimulator/src/main/java/com/has_akh/databasesimulator/Tuple.java
@@ -1,40 +1,44 @@
 package com.has_akh.databasesimulator;
 
-
 import java.util.Map;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
- * @author Hasan Akhtar
- * Represents a row in a table.
- * Responsibilities:
- * - Store values in a structured way
- * - Provide access by column name
- * Fields:
- * values - stores the values in this tuple
+ * @author Hasan Akhtar Represents a row in a table. Responsibilities: - Store
+ * values in a structured way - Provide access by column name Fields: values -
+ * stores the values in this tuple
  */
 public class Tuple {
+
     private String primaryKey;
     private Map<String, Object> values;
-    
+
     public Tuple(Map<String, Object> initialValues, String primaryKey) {
         values = initialValues;
         this.primaryKey = primaryKey;
     }
-    
+
     public void setValues(Map<String, Object> newValues) {
         values = newValues;
     }
-    
+
     public Map<String, Object> getValues() {
         return values;
     }
-    
+
     public String getPrimaryKey() {
         return primaryKey;
     }
+
+    public Object get(String columnName) {
+        return values.get(columnName);
+    }
+
+    public void set(String columnName, Object value) {
+        values.put(columnName, value);
+    }
+
 }
